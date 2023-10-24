9a71d85f49bcd84116b24003d47423a769dee971
jihwan2843 <jihwan2843@gmail.com>
2023-10-24 13:40:13 +0900

    js add

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 console.log("hello");
 alert("hello");
+
+let h2 = document.createElement("h2");
+
+var dom = document.getElementById("dom");
+
+h2.innerText = "My name is h662";
+
+dom.appendChild(h2);
+
+
+
